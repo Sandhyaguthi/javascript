d9295672de2a6b18df5a54b98761f87c3543ae76
Sandhyaguthi <guthisandhya3@gmail.com>
2026-03-12 12:23:48 +0530

    Add files via upload

---
+++ driving.js
@@ -0,0 +1,7 @@
+let age=28;
+let dl=false;
+if(age>18 || dl==true){
+    console.log("you can drive");
+}else{
+    console.log("you con't drive");
+}
---
+++ ifelse.js
@@ -0,0 +1,6 @@
+ let x=100;
+ if(x>10){
+     console.log("x is greater than 10");
+ }else{
+    console.log("x is not greater than 10");
+ }
